ac95df0be4968cac8ea51c2d663a1639ca383ae1
Cube <116672365+ZLCube@users.noreply.github.com>
2023-08-15 22:06:04 -0600

    Update ethernet_status.sh

--- Themes/Parrot/Config/bin/ethernet_status.sh
+++ Themes/Parrot/Config/bin/ethernet_status.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-echo "%{F#8bcc6a} %{F#FFFFFF}$(/usr/sbin/ifconfig ens33 | grep "inet " | awk '{print $2}')%{u-}"
+echo "%{F#8bcc6a} %{F#FFFFFF}$(/usr/sbin/ifconfig eth0 | grep "inet " | awk '{print $2}')%{u-}"
