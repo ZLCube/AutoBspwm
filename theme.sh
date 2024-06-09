3e2ff5f1195cf97ae58fe63700a15ac543b8709a
Cube <116672365+ZLCube@users.noreply.github.com>
2024-06-09 03:48:23 -0400

    Update theme.sh

--- theme.sh
+++ theme.sh
@@ -214,13 +214,52 @@ ZLCube_theme()
     kill -9 -1
 }
 
+Legion_theme()
+{
+	#Damos permisos de ejecución 
+	sudo chmod +x $ruta/Themes/Legion/.p10k.zsh
+	sudo chmod +x $ruta/Themes/Legion/.p10k.zsh-root
+	sudo chmod +x $ruta/Themes/Legion/Config/bspwm/bspwmrc 
+    sudo chmod +x $ruta/Themes/Legion/Config/bspwm/scripts/bspwm_resize 
+    sudo chmod +x $ruta/Themes/Legion/Config/bin/ethernet_status.sh
+    sudo chmod +x $ruta/Themes/Legion/Config/bin/htb_status.sh 
+    sudo chmod +x $ruta/Themes/Legion/Config/bin/htb_target.sh 
+    sudo chmod +x $ruta/Themes/Legion/Config/polybar/launch.sh 
+    sudo chmod +x /usr/local/bin/whichSystem.py 
+    sudo chmod +x /usr/local/bin/screenshot 
+	#Limpiamos viejos dot files
+	sudo rm -rf ~/.p10k.zsh
+	sudo rm -rf /root/.p10k.zsh
+	sudo rm -rf ~/.config/bspwm
+	sudo rm -rf ~/.config/bin
+	sudo rm -rf ~/.config/picom
+	sudo rm -rf ~/.config/polybar
+	sudo rm -rf ~/.config/rofi
+	sudo rm -rf ~/.config/Wallpaper
+ 	sudo rm -rf ~/.config/kitty
+  	sudo rm -rf ~/.config/neofetch
+   	sudo rm -rf ~/.config/wallpapers
+	#Movemos los dot files
+    sudo cp -v $ruta/Themes/Legion/.p10k.zsh ~/.p10k.zsh
+    sudo cp -v $ruta/Themes/Legion/.p10k.zsh-root /root/.p10k.zsh
+    echo "p10k setup"
+	sudo cp -rv $ruta/Themes/Legion/config/* ~/.config/
+	echo "config setup"
+    rofi-theme-selector
+    echo "Legion theme instalado"
+    kill -9 -1
+}
 
-selected_option=$(opciones "Pacman" "Parrot" "S4vi" "Cinnamoroll" "Pink" "ZLCube")
+
+selected_option=$(opciones "Pacman" "Parrot" "S4vi" "Cinnamoroll" "Pink" "ZLCube" "Legion")
 
 
 case "$selected_option" in
     "Pacman")
         Pacman_theme
+        ;;
+    "Legion")
+        Legion_theme
         ;;
     "Parrot")
         Parrot_theme
@@ -234,10 +273,10 @@ case "$selected_option" in
     "Pink")
         Pink_theme
         ;;
+	
     "ZLCube")
         ZLCube_theme
         ;;
-
 
     *)
         echo "Selección inválida."
