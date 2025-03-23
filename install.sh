99a5f1942af3d4ee285584a0860e49bf8f25342d
Cube <116672365+ZLCube@users.noreply.github.com>
2025-03-23 16:23:29 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -14,7 +14,7 @@ sudo apt install -y build-essential git vim libxcb-util0-dev libxcb-ewmh-dev lib
 
 # Instalando Requerimientos para la polybar
 
-sudo apt install -y cmake cmake-data pkg-config python3-sphinx libcairo2-dev libxcb1-dev libxcb-util0-dev libxcb-randr0-dev libxcb-composite0-dev python3-xcbgen xcb-proto libxcb-image0-dev libxcb-ewmh-dev libxcb-icccm4-dev libxcb-xkb-dev libxcb-xrm-dev libxcb-cursor-dev libasound2-dev libpulse-dev libjsoncpp-dev libmpdclient-dev libuv1-dev libnl-genl-3-dev
+sudo apt install -y polybar cmake cmake-data pkg-config python3-sphinx libcairo2-dev libxcb1-dev libxcb-util0-dev libxcb-randr0-dev libxcb-composite0-dev python3-xcbgen xcb-proto libxcb-image0-dev libxcb-ewmh-dev libxcb-icccm4-dev libxcb-xkb-dev libxcb-xrm-dev libxcb-cursor-dev libasound2-dev libpulse-dev libjsoncpp-dev libmpdclient-dev libuv1-dev libnl-genl-3-dev
 
 # Dependencias de Picom
 
